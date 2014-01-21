24da13f6e7b36413fc1b377b03f6824f4d9481dd
Peter Håkansson <pallevante@live.se>
2014-01-21 20:29:27 +0100

    La till lite 'exempel design' för koden.

--- Iris/Main.cpp
+++ Iris/Main.cpp
@@ -1,1 +1,25 @@
-/*Iris*/
+/*Iris
+
+Uppbyggnad av kod - förslag av Peter
+
+
+funktioner följer designen som visas nedan.
+
+int main(){
+	//Kod här.
+}
+
+if(vilkor == 1 &&
+	vilkor == 2 ||
+	vilkor == 3){
+	}
+
+
+variabelnamn börjar med liten bokstav sedan följer "Camel case", medlemsvariabler börjar med bokstaven m.  
+
+Klasser/Enums börjar med stor bokstav
+Instanser av klasser hanteras som variabelnamn dvs börjar med liten bokstav.
+
+
+
+*/
