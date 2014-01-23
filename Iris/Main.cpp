7c32582f33cfe5bd87846b1255ef22b3ad21bf2f
simonbq <bergqvist.simon@gmail.com>
2014-01-23 16:02:10 +0100

    SFML La till SFML med exempelkod i Main

--- Iris/Main.cpp
+++ Iris/Main.cpp
@@ -21,3 +21,28 @@ Klasser/Enums börjar med stor bokstav
 Instanser av klasser hanteras som variabelnamn dvs börjar med liten bokstav.
 
 */
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
