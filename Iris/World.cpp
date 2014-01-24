ecd2e5e6cf0f4e203d85838b04e3329ee75720cc
Peter Håkansson <pallevante@live.se>
2014-01-24 17:28:39 +0100

    Fixade lite head problem.

--- Iris/World.cpp
+++ Iris/World.cpp
@@ -3,15 +3,8 @@
 
 World::World(){}
 
-<<<<<<< HEAD
-=======
-World::World(){
-}
->>>>>>> a6d5e89d94d9433ce489f2064f8b8ce0c9bb1d55
-
 World::~World(){}
 
-<<<<<<< HEAD
 
 void World::run(){
 	sf::RenderWindow window(sf::VideoMode(1024, 768), "Iris");
@@ -32,7 +25,3 @@ void World::run(){
 		window.display();
 	}
 }
-=======
-World::~World(){
-}
->>>>>>> a6d5e89d94d9433ce489f2064f8b8ce0c9bb1d55
