56ef5af7539a52d169e461dc6c2805125ab1b286
simonbq <bergqvist.simon@gmail.com>
2014-01-27 15:02:40 +0100

    Fixade konvention för medlemsvariabler

--- Iris/ResourceManager.hpp
+++ Iris/ResourceManager.hpp
@@ -2,7 +2,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <unordered_map>
-#include <memory>
 
 /* Använd funktionerna för att lägga till nya resurser, och använd enumsen för att separera mellan unika entities/levels/etc. */
 
@@ -16,7 +15,7 @@ public:
 	sf::Texture& getLevel(unsigned int TYPE);
 	sf::Sound& getSound(unsigned int TYPE);
 private:
-	std::unordered_map<unsigned int, sf::Texture*> textures;
-	std::unordered_map<unsigned int, sf::Texture*> levels;
-	std::unordered_map<unsigned int, sf::Sound*> sounds;
+	std::unordered_map<unsigned int, sf::Texture*> mTextures;
+	std::unordered_map<unsigned int, sf::Texture*> mLevels;
+	std::unordered_map<unsigned int, sf::Sound*> mSounds;
 };
