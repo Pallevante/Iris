2b53d0c2900fa12e38f88a6547c4eb308cf3c856
Simon Bergqvist <bergqvist.simon@gmail.com>
2014-01-27 13:53:14 +0100

    Resource Manager sounds La till en map för ljud i ResourceManager. Inkluderade ljudrelaterade filer

--- Iris/ResourceManager.cpp
+++ Iris/ResourceManager.cpp
@@ -1,10 +1,18 @@
 #include "ResourceManager.h"
 
+/* emplace lägger till i mapen om ID't, alltså TYPE, är unikt och inte har något tidigare värde. Vi kan behöva en changeTexture i framtiden. */
+void ResourceManager::addTexture(unsigned int TYPE, sf::Texture &texture){
+	textures.emplace(TYPE, &texture);
+}
+
+void ResourceManager::addSound(unsigned int TYPE, sf::Sound &sound){
+	sounds.emplace(TYPE, &sound);
+}
+
 sf::Texture& ResourceManager::getTexture(unsigned int TYPE){
 	return *textures[TYPE];
 }
 
-/* emplace lägger till i mapen om ID't, alltså TYPE, är unikt och inte har något tidigare värde. Vi kan behöva en changeTexture i framtiden. */ 
-void ResourceManager::addTexture(unsigned int TYPE, sf::Texture &texture){
-	textures.emplace(TYPE, &texture);
+sf::Sound& ResourceManager::getSound(unsigned int TYPE){
+	return *sounds[TYPE];
 }
--- Iris/ResourceManager.h
+++ Iris/ResourceManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <unordered_map>
 #include <memory>
 
@@ -7,8 +8,11 @@
 class ResourceManager{
 public:
 	void addTexture(unsigned int TYPE, sf::Texture &texture);
+	void addSound(unsigned int TYPE, sf::Sound &sound);
 
 	sf::Texture& getTexture(unsigned int TYPE);
+	sf::Sound& getSound(unsigned int TYPE);
 private:
 	std::unordered_map<unsigned int, sf::Texture*> textures;
+	std::unordered_map<unsigned int, sf::Sound*> sounds;
 };
