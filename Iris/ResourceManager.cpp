68d1035abf4c16ed07b2be9f4998ebd3822181d1
simonbq <bergqvist.simon@gmail.com>
2014-01-27 14:42:35 +0100

    Levels i ResourceManager

--- Iris/ResourceManager.cpp
+++ Iris/ResourceManager.cpp
@@ -5,12 +5,20 @@ void ResourceManager::addTexture(unsigned int TYPE, sf::Texture &texture){
 	textures.emplace(TYPE, &texture);
 }
 
+void ResourceManager::addLevel(unsigned int TYPE, sf::Texture &level){
+	textures.emplace(TYPE, &level);
+}
+
 void ResourceManager::addSound(unsigned int TYPE, sf::Sound &sound){
 	sounds.emplace(TYPE, &sound);
 }
 
 sf::Texture& ResourceManager::getTexture(unsigned int TYPE){
 	return *textures[TYPE];
+}
+
+sf::Texture& ResourceManager::getLevel(unsigned int TYPE){
+	return *levels[TYPE];
 }
 
 sf::Sound& ResourceManager::getSound(unsigned int TYPE){
--- Iris/ResourceManager.hpp
+++ Iris/ResourceManager.hpp
@@ -4,15 +4,19 @@
 #include <unordered_map>
 #include <memory>
 
-/* Resource manager som nuvarande enbart kan ta emot texturer. */
+/* Använd funktionerna för att lägga till nya resurser, och använd enumsen för att separera mellan unika entities/levels/etc. */
+
 class ResourceManager{
 public:
 	void addTexture(unsigned int TYPE, sf::Texture &texture);
+	void addLevel(unsigned int TYPE, sf::Texture &level);
 	void addSound(unsigned int TYPE, sf::Sound &sound);
 
 	sf::Texture& getTexture(unsigned int TYPE);
+	sf::Texture& getLevel(unsigned int TYPE);
 	sf::Sound& getSound(unsigned int TYPE);
 private:
 	std::unordered_map<unsigned int, sf::Texture*> textures;
+	std::unordered_map<unsigned int, sf::Texture*> levels;
 	std::unordered_map<unsigned int, sf::Sound*> sounds;
 };
