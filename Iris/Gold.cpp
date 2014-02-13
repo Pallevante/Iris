ddc94909d0e57010fc91e6454d0d3dbe36cd6cce
Peter Håkansson <pallevante@live.se>
2014-02-13 13:53:47 +0100

    La till guldsystemet

---
+++ Iris/Gold.cpp
@@ -0,0 +1,73 @@
+#include "Gold.hpp"
+
+Gold::Gold(float xPosition, float yPosition): 
+
+mSpeed(6),
+mAcceleration(0.5f),
+mIsAlive(true)
+//Måste ändras relativt till bilden.
+{
+	mAnimation = new Animation("resource/textures/entities/gold.png", 100, 8);
+	mAnimation->setPosition(sf::Vector2f(xPosition, yPosition));
+
+}
+
+
+Gold::~Gold(){}
+
+int Gold::getHeight() const{
+	return mAnimation->getSprite().getTextureRect().height;
+}
+
+int Gold::getWidth() const{
+	return mAnimation->getSprite().getTextureRect().width;
+}
+
+Gold::Type Gold::getType() const{
+	return GOLD;
+}
+
+sf::Vector2f Gold::getPosition(){
+	return mAnimation->getSprite().getPosition();
+}
+
+float Gold::getRad() const {
+	float x = mAnimation->getSprite().getTextureRect().width;
+	float y = mAnimation->getSprite().getTextureRect().height;
+
+	return sqrtf(powf(x, 2) + powf(y, 2)) / 2;
+}
+
+void Gold::tick(EntityVector &entities, float dt){
+	move(entities, dt);
+}
+
+/*Du använder en check i collision i world om typen är GOLD sedan hämtar du damage för värdet.*/
+int Gold::getDamage() const{
+	return 1;
+}
+
+void Gold::setDamage(int newDamage){
+	return;
+}
+
+bool Gold::isAlive(){
+	return mIsAlive;
+}
+
+int Gold::collide(Entity *e0, EntityVector &entities){
+	if (e0->getType() == PLAYER){
+		mIsAlive = false;
+		return getDamage();
+	}
+	else
+		return 0;
+}
+
+void Gold::setMovement(Movement movement){
+	mMovement = movement;
+}
+
+void Gold::move(EntityVector &entities, float dt){
+
+}
